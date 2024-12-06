df6283591d1ae41d1456b7fdd6e304c458349264
LebowskiSama <anushvenkat101@gmail.com>
2024-12-06 03:45:52 -0800

    success part 2/2 day 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,6 +1,9 @@
 // Day 1 - Historian Hysteria https://adventofcode.com/2024/day/1
 // figure out the total distance between two given lists
-// distance = distance(left_large. right_large) + distance(left_second_largest, right_second_largest) + ... + distance(left_n_large, right_n_large)
+// part - 1 distance = distance(left_large. right_large) + distance(left_second_largest, right_second_largest) + ... + distance(left_n_large, right_n_large)
+// part - 2 similarity score = sum(each number in left list * frequency of occurence in right list)
+
+use std::collections::HashMap;
 
 fn main() {
     // parse inputs
@@ -8,10 +11,17 @@ fn main() {
     let lines = parser(&data_path);
     // convert to ints
     let (left, right) = split_cast(&lines);
+
+    // part one
     // calculate distance as sum of absolute differences in sorted pairs
     let distance = calc_distance(&left, &right);
-    
-    println!("{}", distance);
+
+    // part two
+    // calculate similarity score
+    let similarity_score = calc_similarity(&left, &right);
+
+    println!("distance: {}", distance);
+    println!("similarity_score: {}", similarity_score);
 }
 
 fn parser(path_to_data: &str) -> Vec<String> {
@@ -46,5 +56,57 @@ fn calc_distance(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
         distance += (ref_left - ref_right).abs();
     }
 
-    distance   
+    distance
+}
+
+fn calc_similarity(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
+    let mut similarity_score: i32 = 0;
+    let mut frequency_map: HashMap<i32, i32> = HashMap::new();
+    for left_ele in left.iter() {
+        if frequency_map.contains_key(left_ele) {
+            similarity_score += left_ele * frequency_map.get(left_ele).unwrap();
+        } else {
+            // calculate frequency
+            let frequency = binary_search_freq(*left_ele, right);
+            // update similarity score
+            similarity_score += left_ele * frequency;
+            // push new frequency for ele into frequency map for future repetitions of same left_ele
+            frequency_map.entry(*left_ele).or_insert(frequency);
+        }
+    }
+    similarity_score
+}
+
+fn binary_search_freq(target: i32, nums: &Vec<i32>) -> i32 {
+    // sorted list => binary search
+    let mut left: usize = 0;
+    let mut right: usize = nums.len();
+    let mut freq: usize = 0;
+
+    while left < right {
+        let mid: usize = left + ((right - left) / 2);
+        if nums[mid] == target {
+            // increment frequency for mid match
+            freq += 1;
+            // dual pointer spread search
+            let mut a = mid - 1;
+            let mut b = mid + 1;
+            while nums[a] == target && a > 0 {
+                // increment frequency for left match
+                freq += 1;
+                a -= 1;
+            }
+            while nums[b] == target && b < nums.len(){
+                // increment frequency for right match
+                freq += 1;
+                b += 1;
+            }
+            return freq as i32
+        } else if target > nums[mid] {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return 0
 }
