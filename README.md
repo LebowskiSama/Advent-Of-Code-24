566faf0ee54e517a8444e2c4fb25b30686b6c951
LebowskiSama <anushvenkat101@gmail.com>
2024-12-07 08:46:50 -0800

    completed day 2 part 2

--- README.md
+++ README.md
@@ -22,3 +22,34 @@ Since we already had the typical input parsing + string operations + sorting alr
 2. Caching of key-value pairs of left_element: frequency for reduced computation and improved efficiency.
 
 Overall the tasks were pretty straightforward and gentle enough for Day-1 and it was fun to be able to handle these. Can improvements be made here? For sure, we can bring in multithreading for the extended portion of the binary search and a lot of other such improvements that would border on over-engineering.
+
+# Day 2 -  Red-Nosed Reports
+## Part 1 - N safe reports
+Date: 6 Nov 2024
+Time to solve: 50 mins
+
+This one doesn't disappoint either. Fun puzzle here. Essentially, you're to judge reports (arrays) and how they progress. A report is considered a safe report if:
+1. Constraint 1: The elements in the list are strictly ascending or strcitly descending.
+2. Constraint 2: If the absolute difference between any two successive elements in a report is at least 1 and utmost 3.
+
+The problem was broken down into:
+1. Input parsing: reading lines from a text file.
+2. String operations: splitting and casting strings into integers.
+3. Array traversal and conditional statements: 
+    - Calculate ascending or descending from the first two elements and store it as an integer (-1 for descending, 0 for 0, 1 for ascending). Use this to keep constraint 1 satisfied for ever difference calculated.
+    - Calculate the successive differences during traversal and check for constraint 2. Break if not satisfied, increment safe reports if satisfied.
+
+## Part 2 - Dampened accounted safety
+Date: 7 Nov 2024
+Time to solve: 130 mins
+
+Alright, we now have a problem that gets you thinking.
+As an extension of part 1, you can now remove one at most to deem a report safe.
+- 1 ~~3~~ 2 4 5: becomes safe by removing 3 at level 2 - because 1 -> 3 is ascending but 3 -> 2 is descending, we're removing here to maintain ordering.
+- 8 6 ~~4~~ 4 1: again, removing 4 here maintains ordering because 4 -> 4 is neither ascending nor descending.
+
+The gist here is, you either remove a level to  
+a. maintain ordering or   
+b. ensure differences are within safety levels (at least 1, at most 3).
+
+Bottom line: a brute force approach was taken, where the constraints are checked after popping each element of the array, increasing memory complexity by duplicating on a report level and compounding time complexity by iterating at least n<sup>2</sup> per report. Sure enough, a more optimal solution must exist.
--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,13 +1,17 @@
 // Day 1 - Red-Nosed Reports
 // part 1 - figure out if values are either strictly increasing or decreasing and inter integer distance is at least 1 and utmost 3
+// part 2 - part 1 with problem dampener, if removing any one level makes it safe, then the report can be considered safe
 
 fn main() {
     // parse input data
     let data_path: String = String::from("./input/input.txt");
-    let reports = parser(&data_path);
+    let mut reports = parser(&data_path);
     // calculate total safe reports present
     let n_safe: i32 = calc_safety(&reports);
     println!("number of safe reports: {}", n_safe);
+
+    let n_safe_damp: i32 = calc_safety_dampened(&mut reports);
+    println!("number of dampened safe reports: {}", n_safe_damp);
 }
 
 fn parser(data_path: &str) -> Vec<Vec<i32>> {
@@ -42,7 +46,7 @@ fn calc_safety(reports: &Vec<Vec<i32>>) -> i32 {
         let asc_desc = (report[1] - report[0]).signum();
         
         while i < n && asc_desc != 0 {
-            // calculate absolute difference between elements
+            // calculate difference between successive elements
             let diff = report[i] - report[i - 1];
             if diff.signum() == asc_desc && diff.abs() >= 1 && diff.abs() <= 3 {
                 i += 1;
@@ -57,3 +61,55 @@ fn calc_safety(reports: &Vec<Vec<i32>>) -> i32 {
     
     n_safe
 }
+
+
+fn all_satisfied(vec: &Vec<i32>) -> bool {
+    let n = vec.len();
+    let mut i: usize = 1;
+    let asc_desc: i32 = (vec[1] - vec[0]).signum();
+
+    if asc_desc == 0 { return false };
+    
+    while i < n {
+        let diff = vec[i] - vec[i - 1];
+        if diff.signum() != asc_desc || diff.abs() < 1 || diff.abs() > 3 {
+            return false
+        } else {
+            i += 1;
+        }
+    }
+    return true
+}
+
+fn calc_safety_dampened(reports: &mut Vec<Vec<i32>>) -> i32 {
+    // a report is considered safe if
+    // 1. the progression is strictly ascending or descending
+    // 2. the difference between two consecutive readings is at least 1 and utmost 3
+    // 3. however, as the added catch here report is still considered safe if removing just any one level makes it safe
+    // we do this by checking if the entire
+
+    let mut n_safe = 0;
+
+    for report in reports.iter_mut() {
+        if !all_satisfied(report) {
+            let n = report.len();
+            let mut i: usize = 0;
+
+            while i < n {
+                let mut report_ = report.clone();
+                report_.remove(i);
+                if all_satisfied(&report_) {
+                    n_safe += 1;
+                    break;
+                } else {
+                    i += 1;
+                }
+            }
+       } else {
+        n_safe += 1;
+       }
+    }
+    
+    n_safe
+}
+
